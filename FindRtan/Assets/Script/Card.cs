85ed3f193bbdcb59b3055951f2fbb5aef07fa222
DoHyeon8214 <dodam8214@naver.com>
2024-04-16 18:53:13 +0900

    추가기능 구현 - 첫번째 카드 고르고 5초 카운트 다운

--- FindRtan/Assets/Script/Card.cs
+++ FindRtan/Assets/Script/Card.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class Card : MonoBehaviour
      public AudioClip clip;
      AudioSource audioSource;
 
+
     void Start()
     {
      audioSource = GetComponent<AudioSource>();
@@ -24,7 +26,7 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Setting(int number)
@@ -44,12 +46,14 @@ public class Card : MonoBehaviour
 
         if (GameManager.Instance.firstCard == null )
         {
-         GameManager.Instance.firstCard = this;
+            GameManager.Instance.firstCard = this;
+            
         }
         else
         {
             GameManager.Instance.secondCard = this;
             GameManager.Instance.Matched();
+
         }
     }
 
@@ -74,4 +78,6 @@ public class Card : MonoBehaviour
         front.SetActive(false);
         clickBack.SetActive(true);
     }
+
+
 }
--- FindRtan/Assets/Script/GameManager.cs
+++ FindRtan/Assets/Script/GameManager.cs
@@ -31,9 +31,17 @@ public class GameManager : MonoBehaviour
 
     float time = 0.0f;
 
-    private void Awake() 
+
+    //첫번 째 카드 뒤집을 때 카운트 다운 기능(권도현)
+    public Text countDownText;
+    public GameObject countDownPanel;
+
+    //5초 세팅
+    private float countDownTime = 5f;
+
+    private void Awake()
     {
-        if(Instance ==null)
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -62,42 +70,74 @@ public class GameManager : MonoBehaviour
 
         if (time > 30f)
         {
-         endTitle.SetActive(true);
-          endTxt.SetActive(true);
-          Time.timeScale = 0.0f;
+            endTitle.SetActive(true);
+            endTxt.SetActive(true);
+            Time.timeScale = 0.0f;
         }
+
+        //첫번 째 카드를 선택했다면
+        if (firstCard != null)
+        {
+            //카운트 다운 시작
+            StartCountdown();
+        }
+
+
     }
 
     public void Matched()
     {
-       if(firstCard.idx == secondCard.idx)
-       {
-          audioSource.PlayOneShot(clip);
+        if (firstCard.idx == secondCard.idx)
+        {
+            audioSource.PlayOneShot(clip);
 
-         firstCard.DestroyCard();
-         secondCard.DestroyCard();
-         cardCount -= 2;
-         if(cardCount == 0)
-         {
-            Time.timeScale = 0f;
-            endTitle.SetActive(true);
-            endTxt.gameObject.SetActive(true);
-         }
-       }
-       else
-       {
-        audioSource.PlayOneShot(isWrong);
-         matchFailTxt.SetTrigger("Fail");
-         firstCard.CloseCard();
-         secondCard.CloseCard();
-       }
-
-        firstCard = null;
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            cardCount -= 2;
+            if (cardCount == 0)
+            {
+                Time.timeScale = 0f;
+                endTitle.SetActive(true);
+                endTxt.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            audioSource.PlayOneShot(isWrong);
+            matchFailTxt.SetTrigger("Fail");
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+        //카드 짝 맞는 여부 상관없이 두번 째 카드까지 고르는 작업이 진행됬을 때는 카운트 다운 멈춤
+        HideCountDown();
         secondCard = null;
 
         count++;
         countTxt.text = count.ToString();
-    } 
+    }
+    public void StartCountdown()
+    {
+        // 0초 되기 전 까지는 1초씩 감소
+        if (countDownTime > 0f)
+        {
+            countDownPanel.SetActive(true);
+            countDownTime -= Time.deltaTime;
+            countDownText.text = countDownTime.ToString("F0");
+        }
+        else
+        {
+            //0초 되면 화면 사라짐
 
-    
+            HideCountDown();
+
+        }
+    }
+    public void HideCountDown()
+    {
+        countDownPanel.SetActive(false);
+        firstCard.CloseCard();
+        firstCard = null;
+        countDownTime = 5f;
+    }
+
 }
